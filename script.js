c7e9c3848b3a64bd502ad878f8e4249521d42c99
Akram_ch <akramchaouache@gmail.com>
2022-08-05 11:51:36 +0200

    made intro responsive

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const toggleBtn = document.getElementsByClassName('toggle-button')[0];
 const navLinks = document.getElementsByClassName('nav-links')[0];
+const imgBox = document.getElementsByClassName('imgbox')[0];
 
 toggleBtn.addEventListener('click', ()=>{
     navLinks.classList.toggle('active');
+    imgBox.classList.toggle('hidden');
 })
